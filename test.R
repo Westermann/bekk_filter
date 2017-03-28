7efaf5fbf9b472c96b050b3e900e64652fa021e6
Westermann <westermann93@googlemail.com>
2017-03-28 17:36:30 +0200

    Update test function

--- test.R
+++ test.R
@@ -1,10 +1,12 @@
 dyn.load('./C/bekk_log_lik.so')
-source('./R/bekk_model.R')
+source('./R/scalar_beck.R')
 
 y <- read.csv('../ganter/data/Problemsets Data (Tickers)/all.csv')
 y <- apply(y,c(2),diff)
 y[is.na(y)] <- 0
 
-fit <- scalar.bekk.fit(as.matrix(y[,2:4]),opts=list(fit = TRUE))
+# scalar.bekk.filter(as.matrix(y[1:10,2:4]),c(0.9,0.5),k=1)
+fit <- scalar.bekk.fit(as.matrix(y[,2:4]),opts=list(fit = TRUE,
+                                                    lags = 5))
 print(fit$param)
 print(fit$obj)
